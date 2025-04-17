7f78400e0e0d2338455edc73d03025494bf48cd9
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2025-04-17 22:40:11 +0800

    final draft

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,9 @@ import logo from '../assets/logo.svg'
 export default function Logo() {
   return (
     <div className='logo h-16 w-45'>
+      <a href='/'>
       <img src={logo} />
+      </a>
     </div>
   )
 }
