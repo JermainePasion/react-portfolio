fbda3d29264a934f607f676582af20cec16db435
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2025-04-18 22:05:38 +0800

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 import App from './App';
 import './index.css'
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
