8107c6f3180a9dbf6bdd4d06f2362b30d946365e
Jermaine Riley M. Pasion <jmpasion@student.hau.edu.ph>
2026-05-04 20:48:31 +0800

    add: sitemap

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(),
+    sitemap({
+      hostname: 'https://squirkle-website.vercel.app'
+    })
+  ],
   base: process.env.VITE_BASE_PATH || "/react-portfolio"
   
 })
